4d70a0fcb34324b7a2aeb991f74ff6e1d97989db
Jeff Sheets <sheets.jeff@gmail.com>
2021-09-29 21:56:36 -0500

    Created an extremely basic main source file

---
+++ src/main.cc
@@ -0,0 +1,14 @@
+// This document is licensed according to the LGPL v2.1 license
+// Consult the LICENSE file in the root project directory for details
+
+#ifndef NEBULA_MAIN_CC
+#define NEBULA_MAIN_CC
+
+int main (
+  int argc,
+  char *argv[])
+{
+  return 0;
+}
+
+#endif //NEBULA_MAIN_CC
