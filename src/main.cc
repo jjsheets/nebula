69e4451b19b1ef4d07490ada0fbeb37fe802aa70
Jeff Sheets <sheets.jeff@gmail.com>
2021-10-01 15:05:01 -0500

    Added basic game window handling code

--- src/main.cc
+++ src/main.cc
@@ -4,8 +4,41 @@
 #ifndef NEBULA_MAIN_CC
 #define NEBULA_MAIN_CC
 
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+void error_callback(int error, const char *description)
+{
+  std::cerr << "Error: " << description << std::endl;
+}
+
+static void key_callback(
+    GLFWwindow *window, int key, int scancode, int action, int mods)
+{
+  if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
+    glfwSetWindowShouldClose(window, GLFW_TRUE);
+  }
+}
+
 int main(int argc, char *argv[])
 {
+  if (!glfwInit()) {
+    return -1;
+  }
+  glfwSetErrorCallback(error_callback);
+  glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+  GLFWwindow *window = glfwCreateWindow(640, 480, "Nebula", NULL, NULL);
+  if (!window) {
+    glfwTerminate();
+    return 0;
+  }
+  glfwSetKeyCallback(window, key_callback);
+  while (!glfwWindowShouldClose(window)) {
+    glfwPollEvents();
+  }
+  glfwDestroyWindow(window);
+  glfwTerminate();
   return 0;
 }
 
