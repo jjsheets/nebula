b6572d6fe2b3de74c3bf1851f0b3c75043cab660
Jeff Sheets <sheets.jeff@gmail.com>
2021-10-01 22:25:21 -0500

    Add main file for the unit tester

---
+++ src/test.cc
@@ -0,0 +1,10 @@
+// This document is licensed according to the LGPL v2.1 license
+// Consult the LICENSE file in the root project directory for details
+
+#ifndef NEBULA_MAIN_CC
+#define NEBULA_MAIN_CC
+
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "doctest.h"
+
+#endif // NEBULA_MAIN_CC
