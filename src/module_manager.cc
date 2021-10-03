546001ee45adf0c85b85af25f8f0570a959ac4e1
Jeff Sheets <sheets.jeff@gmail.com>
2021-10-03 15:03:12 -0500

    Changed from relative path usage to absolute
    Now all module paths are converted to absolute paths.

--- src/module_manager.cc
+++ src/module_manager.cc
@@ -12,6 +12,7 @@
 #include "loguru.hpp"
 
 // Folder Handling includes
+#include <filesystem>
 #include "platform_folders.h"
 
 SCENARIO("class moduleManager")
@@ -22,9 +23,11 @@ SCENARIO("class moduleManager")
     WHEN("a non-local mod path is added")
     {
       modManager.addModulePath("data", true, false);
-      THEN("getModulePaths returns a relative file path")
+      THEN("getModulePaths returns a path in the current working directory")
       {
-        REQUIRE(modManager.getModulePaths() == "./data");
+        REQUIRE(modManager.getModulePaths()
+                == (const std::string &)(std::filesystem::current_path())
+                       + "/data");
       }
     }
     WHEN("a local mod path is added")
@@ -44,14 +47,20 @@ namespace nebula {
 void moduleManager::addModulePath(
     const std::string &path, bool autoLoad, bool local)
 {
+  LOG_SCOPE_FUNCTION(INFO);
   if (local) {
     _modPaths.emplace(_modPaths.end(),
         sago::getSaveGamesFolder2() + "/" + path,
         autoLoad,
         local);
   } else {
-    _modPaths.emplace(_modPaths.end(), "./" + path, autoLoad, local);
+    _modPaths.emplace(_modPaths.end(),
+        (const std::string &)(std::filesystem::current_path()) + "/" + path,
+        autoLoad,
+        local);
   }
+  LOG_S(INFO) << "Added Module Path: " << _modPaths.back()._path
+              << (autoLoad ? " (auto load)" : "");
 }
 
 std::string moduleManager::getModulePaths()
