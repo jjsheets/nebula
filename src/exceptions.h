85d33ae79e7768b763c9d2e920508634bb95ca49
Jeff Sheets <jjsheets@users.noreply.github.com>
2021-10-06 18:22:25 -0500

    Create glfwException class and add to engine.cc (#25)
    Fixes #19

---
+++ src/exceptions.h
@@ -0,0 +1,43 @@
+// This document is licensed according to the LGPL v2.1 license
+// Consult the LICENSE file in the root project directory for details
+
+#ifndef NEBULA_EXCEPTIONS_H
+#define NEBULA_EXCEPTIONS_H
+
+#include <stdexcept>
+#include <string>
+
+class glfwException : public std::runtime_error {
+  std::string _description;
+  int _code;
+
+public:
+  explicit glfwException() : std::runtime_error("")
+  {
+    const char *_desc;
+    _code        = glfwGetError(&_desc);
+    _description = _desc;
+  }
+  explicit glfwException(const glfwException &other) noexcept
+      : std::runtime_error("")
+  {
+    _description = other._description;
+    _code        = other._code;
+  }
+  virtual const char *what() const noexcept
+  {
+    return _description.c_str();
+  }
+  glfwException &operator=(const glfwException &other) noexcept
+  {
+    _description = other._description;
+    _code        = other._code;
+    return *this;
+  }
+  bool operator==(int other) noexcept
+  {
+    return _code == other;
+  }
+};
+
+#endif // NEBULA_EXCEPTIONS_H
