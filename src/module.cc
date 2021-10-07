3eb64f2832784d5f8fddf2b4124794f349e6e132
Jeff Sheets <sheets.jeff@gmail.com>
2021-10-07 03:07:07 -0500

    First pass at the dependency algorithm
    Will need to setup some tests to verify this. Maybe break down the 
    asteroids game into a set of modules: Game field (with location, mobile, 
    and the systems to update just those), Asteroids (colliders, asteroids, 
    and maybe explosions), Player (player ship and bullet components, along 
    with the code to handle score and life).

--- src/module.cc
+++ src/module.cc
@@ -2,11 +2,46 @@
 // Consult the LICENSE file in the root project directory for details
 
 #include "module.h"
+#include <stdexcept>
+#include <algorithm>
+
+#define NEBULA_SEMVER "0.1.0"
 
 namespace nebula {
 
-module::module(const std::string &path) : _rootPath(path) { }
+module::module(const std::string &path) : _rootPath(path)
+{
+  // read the manifest file for the module
+}
+
+// TODO: Add constructors to allow Map to work directly with the module class,
+// Alternatively, add the vector back in, and use map only with the identifier?
 
 module::~module() { }
+
+std::set<std::string> module::_resolved;
+std::set<std::string> module::_unresolved;
+std::map<std::string, module> module::_modules;
+
+void module::resolve() { }
+
+void module::resolve(const module &mod)
+{
+  if (_resolved.find(mod._identifier) != _resolved.end()) {
+    return;
+  }
+  _unresolved.insert(mod._identifier);
+  std::for_each(
+      mod._dependencies.begin(), mod._dependencies.end(), [](std::string dep) {
+        if (_resolved.find(dep) == _resolved.end()) {
+          if (_unresolved.find(dep) != _resolved.end()) {
+            throw std::runtime_error("Circular dependency detected!");
+          }
+          resolve(_modules[dep]);
+        }
+      });
+  _resolved.insert(mod._identifier);
+  _unresolved.erase(mod._identifier);
+}
 
 } // namespace nebula
--- src/module.h
+++ src/module.h
@@ -5,16 +5,32 @@
 #define NEBULA_MODULE_H
 
 #include <string>
+#include <set>
+#include <vector>
+#include <map>
 
 namespace nebula {
 
 class module {
 private:
+  static std::set<std::string> _resolved;
+  static std::set<std::string> _unresolved;
+
   std::string _rootPath;
+  std::string _identifier;
+  std::string _name;
+  std::string _tags;
+  std::vector<std::string> _dependencies;
+
+  static void resolve(const module &mod);
 
 public:
+  static std::map<std::string, module *> _modules;
+
   module(const std::string &path);
   ~module();
+
+  static void resolve();
 };
 
 } // namespace nebula
